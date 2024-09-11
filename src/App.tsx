5b732b21d7257a0a09e8e07e39eb6ef1f9cd485e
조유진 <uzinfamily@snu.ac.kr>
2024-09-11 10:50:51 +0900

    feat: ui skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
 import './App.css';
+import './index.css';
 
-import { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import viteLogo from '/vite.svg';
+import FaqPage from './pages/FaqPage.tsx';
+import FavPage from './pages/FavPage.tsx';
+import IntroPage from './pages/IntroPage.tsx';
+import Mobile from './pages/Mobile.tsx';
 
-import reactLogo from './assets/react.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route path="/fav" element={<FavPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="/mobile" element={<Mobile />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/FaqPage.tsx
@@ -0,0 +1,59 @@
+import './../index.css';
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FAQSection } from './Faq';
+
+const FavPage: React.FC = () => {
+  const navigator = useNavigate();
+
+  return (
+    <div className="w-screen h-screen flex flex-col bg-gradient-to-b from-[#ECF2FF] to-[#FFF4D2]">
+      <div className="w-full h-full flex flex-row justify-center items-center">
+        {/* Tag */}
+        <div className="flex flex-col justify-end">
+          <div className="flex flex-col mb-6">
+            <div className="w-15 h-[23px]  bg-[#4694F0] drop-shadow-md translate-x-1.5 cursor-pointer"></div>
+            <div className="w-15 h-28 border bg-white vertical-text text-center text-lg font-NPSfontBold font-black pl-2 text-[#4694F0] drop-shadow-lg translate-x-1.5 cursor-pointer">
+              기본정보
+            </div>
+          </div>
+          <div className="flex flex-col ml-2 mb-6 hover:-translate-x-2">
+            <div className="w-12 h-[23px]  bg-[#7B8086] drop-shadow-md translate-x-2 cursor-pointer"></div>
+            <div
+              className="w-12 h-28 border bg-white vertical-text text-center text-lg font-extrabold font-NPSfontBold pl-2 text-[#7B8086] drop-shadow-md translate-x-2 cursor-pointer"
+              onClick={() => {
+                navigator('/fav');
+              }}
+            >
+              추가정보
+            </div>
+          </div>
+          <div className="flex flex-col ml-2 mb-6 hover:-translate-x-2">
+            <div className="w-12 h-[23px]  bg-[#7B8086] drop-shadow-md translate-x-2 cursor-pointer"></div>
+            <div
+              className="w-12 h-28 border bg-white vertical-text text-center text-lg font-extrabold font-NPSfontBold pl-2 text-[#7B8086] drop-shadow-md translate-x-2 cursor-pointer"
+              onClick={() => {
+                navigator('/faq');
+              }}
+            >
+              FAQ
+            </div>
+          </div>
+        </div>
+        {/* Content */}
+        <div className="bg-white text-black w-4/5 h-5/6 flex flex-col justify-center items-center rounded-xl drop-shadow-xl gap-4">
+          <div className="w-11/12 h-16 cursor-default bg-gray-200 rounded-lg text-[#5C5C5C] text-lg font-semibold font-PretendardRegular flex justify-center items-center">
+            조유진 (자유전공학부 19학번 (국어국문학, 컴퓨터공학 전공))
+          </div>
+          <div className="w-[90%] h-[480px] pb-8 m-4 overflow-y-scroll auto-rows-[90%] place-items-center gap-y-10">
+            <FAQSection />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavPage;
